f20516b39d9aada6b47bbfa25eedfba60d064f9e
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-29 14:22:24 +0200

    added button ui

---
+++ src/components/ui/button.tsx
@@ -0,0 +1,56 @@
+import * as React from "react"
+import { Slot } from "@radix-ui/react-slot"
+import { cva, type VariantProps } from "class-variance-authority"
+
+import { cn } from "@/lib/utils"
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-2xl text-base font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-border focus-visible:ring-border/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive cursor-pointer",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-primary text-white border-2 border-secondary hover:bg-secondary",
+        destructive:
+          "bg-destructive text-white border-2 border-destructive hover:bg-destructive/70",
+        outline:
+          "bg-transparent border-2 border-foreground hover:bg-foreground hover:text-white",
+        link: "text-primary underline-offset-4 hover:underline hover:text-secondary",
+      },
+      size: {
+        default: "px-4 py-2",
+        sm: "text-sm px-3 py-1.5 gap-1.5",
+        lg: "px-6 py-3",
+        icon: "size-9",
+        link: "p-0"
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+)
+
+function Button({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<"button"> &
+  VariantProps<typeof buttonVariants> & {
+    asChild?: boolean
+  }) {
+  const Comp = asChild ? Slot : "button"
+
+  return (
+    <Comp
+      data-slot="button"
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    />
+  )
+}
+
+export { Button, buttonVariants }
