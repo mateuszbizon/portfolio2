45db7ffc5f3cb11ea53805522433e8808630534e
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-05-30 15:27:48 +0200

    changed main font classname instead of variable

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${montserrat.variable}`}
+        className={`${montserrat.className}`}
       >
         {children}
       </body>
