3f188d8bccc6ff7e2791818df2df807db15bfff6
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-06-03 14:14:27 +0200

    added skills section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import AboutSection from "@/components/sections/AboutSection";
 import ExperienceSection from "@/components/sections/ExperienceSection";
 import HeroSection from "@/components/sections/HeroSection";
+import SkillsSection from "@/components/sections/SkillsSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
         <HeroSection />
         <AboutSection />
         <ExperienceSection />
+        <SkillsSection />
     </main>
   );
 }
---
+++ src/components/sections/SkillsSection.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from '../ui/container'
+
+function SkillsSection() {
+  return (
+    <section className='py-section-padding bg-[#DDF1F9]'>
+        <Container>
+            <p className='section-name-text'>skills</p>
+            <h2 className='heading2 text-center mb-5'>My expertise</h2>
+        </Container>
+    </section>
+  )
+}
+
+export default SkillsSection
