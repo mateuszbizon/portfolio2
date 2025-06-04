1b52074840f8ada20baf5beee1db4e27beaf02a7
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-06-04 14:40:57 +0200

    added footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import AboutSection from "@/components/sections/AboutSection";
 import ExperienceSection from "@/components/sections/ExperienceSection";
+import Footer from "@/components/sections/Footer";
 import HeroSection from "@/components/sections/HeroSection";
 import SkillsSection from "@/components/sections/SkillsSection";
 
@@ -10,6 +11,7 @@ export default function Home() {
         <AboutSection />
         <ExperienceSection />
         <SkillsSection />
+        <Footer />
     </main>
   );
 }
---
+++ src/components/sections/Footer.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Container from '../ui/container'
+import { Button } from '../ui/button'
+import Link from 'next/link'
+import { EMAIL } from '@/constants'
+import { Github, Linkedin, Send } from 'lucide-react'
+
+function Footer() {
+  return (
+    <footer className='bg-foreground text-foreground-2 pt-section-padding pb-10'>
+        <Container>
+            <div className='flex flex-col items-center gap-8'>
+                <h2 className='heading2 text-center'>Do <span className='text-primary'>you</span> have any questions?</h2>
+                <p className='text-lg text-center'>Reach out to me now and let's discuss how we can change the digital world for the better</p>
+                <Button asChild>
+                    <Link href={`mailto:${EMAIL}`}>
+                        Let's get in touch <Send />
+                    </Link>
+                </Button>
+            </div>
+
+            <div className='flex flex-col md:flex-row md:justify-between items-center gap-5 mt-10'>
+                <p>{/*"©"*/} {new Date().getFullYear()} Mateusz Bizoń</p>
+                <div className='flex items-center gap-3 order-first md:order-none'>
+                    <Button size={"icon"} asChild>
+                        <Link href={"/"}>
+                            <Github />
+                        </Link>
+                    </Button>
+                    <Button size={"icon"} asChild>
+                        <Link href={"/"}>
+                            <Linkedin />
+                        </Link>
+                    </Button>
+                </div>
+            </div>
+        </Container>
+    </footer>
+  )
+}
+
+export default Footer
