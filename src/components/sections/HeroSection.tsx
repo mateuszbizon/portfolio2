23a2f37c26516aec64e166d3d9d477f48ccbf656
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-06-05 19:41:07 +0200

    added spotlight to hero

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Container from '../ui/container'
 import { Button } from '../ui/button'
 import { TextGenerateEffect } from '../ui/text-generate-effect'
+import { Spotlight } from '../ui/spotlight'
 
 function HeroSection() {
     const headWords = "Hi! I'm Matthew, a Next.js Developer"
 
   return (
-    <header className='h-screen radial-gradient'>
+    <header className='relative h-screen radial-gradient overflow-hidden'>
+        <Spotlight className="-top-20 left-0 md:-top-20 md:left-60" fill='white' />
         <Container className='max-w-[700px]'>
             <div className='flex flex-col justify-center gap-8 h-full text-center'>
                 <h1 className='text-white heading1'><TextGenerateEffect words={headWords} /></h1>
