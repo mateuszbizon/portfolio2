30e266374e093515aca0ecfda70ab9b64b468906
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-06-05 12:00:16 +0200

    added nav items

---
+++ src/constants/navItems.ts
@@ -0,0 +1,18 @@
+export const NAV_ITEMS = [
+    {
+        label: "About",
+        link: "#about"
+    },
+    {
+        label: "Experience",
+        link: "#experience"
+    },
+    {
+        label: "Skills",
+        link: "#skills"
+    },
+    {
+        label: "Contact",
+        link: "#contact"
+    }
+]
