d625b37fc5bf0485dd5a55204458d08c7ab215b5
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-06-06 13:58:08 +0200

    changed hero paragraph text

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -14,7 +14,7 @@ function HeroSection() {
         <Container className='max-w-[700px]'>
             <div className='flex flex-col justify-center gap-8 h-full text-center'>
                 <h1 className='text-white heading1'><TextGenerateEffect words={headWords} /></h1>
-                <p className='bigger-text text-foreground-2'>A front-end developer based in Poland</p>
+                <p className='bigger-text text-foreground-2'>Creative front-end developer focused on user-friendly interfaces</p>
                 <div>
                     <Button size={"lg"} asChild>
                         <Link href={"#experience"}>
