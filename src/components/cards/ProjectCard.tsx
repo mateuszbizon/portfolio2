f198330d354f6e424383787b6720f957ea438373
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-06-06 10:42:47 +0200

    changed project card as link

--- src/components/cards/ProjectCard.tsx
+++ src/components/cards/ProjectCard.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Image from 'next/image'
-import { Button } from '../ui/button'
 import Link from 'next/link'
 import { ExternalLink } from 'lucide-react'
 import { Project } from '@/types'
@@ -14,27 +13,25 @@ function ProjectCard({ project }: ProjectCardProps) {
     const emptyTechs = project.technologies.length < 1
 
   return (
-    <div className='rounded-2xl space-y-5'>
-        <figure className='aspect-video relative rounded-xl overflow-hidden'>
-            <Image src={project.image} alt={project.imageAlt} fill />
-        </figure>
+    <Link href={project.link} target='_blank' className='hover:rotate-2 transition-transform duration-200'>
+        <div className='rounded-2xl space-y-5'>
+            <figure className='aspect-video relative rounded-xl overflow-hidden'>
+                <Image src={project.image} alt={project.imageAlt} fill />
+            </figure>
 
-        <div className='space-y-2'>
-            <h3 className='bigger-text'>{project.title}</h3>
-            <p className='text-lg'>{project.description}</p>
-        </div>
-
-        <div className={`flex flex-col ${emptyTechs ? "md:flex-row-reverse" : "md:flex-row"} items-center justify-between gap-1`}>
-            <div className='flex'>
-                <AnimatedTooltip items={project.technologies} />
+            <div className='space-y-2'>
+                <h3 className='bigger-text'>{project.title}</h3>
+                <p className='text-lg'>{project.description}</p>
             </div>
-            <Button variant={"link"} size={"link"} asChild>
-                <Link href={project.link} className='text-lg font-medium text-white' target='_blank'>
-                    {project.isLive ? "Check Live Site" : "Project Github"} <ExternalLink className='size-5' />
-                </Link>
-            </Button>
+
+            <div className={`flex flex-col ${emptyTechs ? "md:flex-row-reverse" : "md:flex-row"} items-center justify-between gap-1`}>
+                <div className='flex'>
+                    <AnimatedTooltip items={project.technologies} />
+                </div>
+                <p className='flex items-center gap-2 font-medium'>{project.isLive ? "Check Live Site" : "Project Github"} <ExternalLink className='size-5' /></p>
+            </div>
         </div>
-    </div>
+    </Link>
   )
 }
 
