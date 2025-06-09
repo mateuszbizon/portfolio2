78df6896e6ab7dce157dd8a8357a12198e66d563
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-06-09 17:28:36 +0200

    changed message ui

--- src/components/common/AskAIChat.tsx
+++ src/components/common/AskAIChat.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import React, { KeyboardEvent } from 'react'
-import { useChat } from "@ai-sdk/react"
+import { useChat, } from "@ai-sdk/react"
 import { Button } from '../ui/button'
 import { Send } from 'lucide-react'
 import { Textarea } from '../ui/textarea'
@@ -28,12 +28,12 @@ function AskAIChat({ isChatOpen }: AskAIChatProps) {
   return (
     <div className={`fixed bottom-24 right-5 flex flex-col ${isChatOpen ? "w-[280px] sm:w-[600px] h-[350px] sm:h-[400px]" : "size-0 pointer-events-none"} rounded-2xl border-2 bg-background overflow-hidden transition-all duration-300`}>
         <div className='bg-secondary p-2 text-center'>
-            <p>Ask AI about me</p>
+            <p className='text-lg font-medium'>Ask AI about me</p>
         </div>
-        <div className='p-2 grow space-y-5 overflow-y-auto no-visible-scrollbar'>
+        <div className='flex flex-col p-2 grow gap-5 overflow-y-auto no-visible-scrollbar'>
             {messages.map(message => (
-                <div key={message.id}>
-                    {message.content}
+                <div key={message.id} className={`w-fit max-w-[calc(100%-2rem)] rounded-xl py-2 px-4 ${message.role === "user" ? "self-end bg-secondary" : "bg-muted/50"}`}>
+                    <p>{message.content}</p>
                 </div>
             ))}
         </div>
